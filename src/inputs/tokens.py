7eb5a0ecfafc2a0fc32f7e2838338fc8a4ae2a02
Ash <partovi.code@gmail.com>
2026-02-15 18:10:35 +0330

    feat+chor: change the assistant file name

--- src/inputs/tokens.py
+++ src/inputs/tokens.py
@@ -58,10 +58,10 @@ def token_manager(tokens_dict):
         return token_list[0]
     
     # Read current token index from file
-    index = int(read_txt_file(filename="inputs/token_manager_assist")[0])
+    index = int(read_txt_file(filename="inputs/token_manager_index_assist")[0])
     token = token_list[index]
     # If was equal "last_token" will be zero
     index = (index + 1) % length_token_list
     # Update and save index in the file
-    write_in_file(file_path="inputs/token_manager_assist", input_item=index, writing_mode="w")
+    write_in_file(file_path="inputs/token_manager_index_assist", input_item=index, writing_mode="w")
     return token
