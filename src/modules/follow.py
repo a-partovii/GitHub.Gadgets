2c3fe7244fa156152b2445eabadbef987a3539f6
Ash <partovi.code@gmail.com>
2026-02-16 19:49:28 +0330

    feat_newFile: add [`follow.py`] major module

---
+++ src/modules/follow.py
@@ -0,0 +1,22 @@
+from inputs.tokens import primary_token, token_manager, headeres
+from modules.delay import delay_and_super_delay
+import requests as req
+
+total = 0
+def follow(username:str):
+    global total
+    url = f"https://api.github.com/user/following/{username}"
+    headers = headeres(token_manager(primary_token))
+    try:
+        response = req.put(url, headers=headers, timeout=5)
+        total += 1
+        try:
+            delay_and_super_delay(message= f'User: {username} followed, Total Follow: "{total}", Token RateLimit Remaining: "{response.headers["X-RateLimit-Remaining"]}"',
+                                min=5,
+                                max=15)
+        except:
+            pass
+        return response
+        
+    except req.RequestException:
+        return None
