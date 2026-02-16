a3b36cec3852b27e15d13016ad93ebee8c154e15
Ash <partovi.code@gmail.com>
2026-02-16 12:32:43 +0330

    feat_dev: now files save in chunks

--- src/modules/get_followers_list.py
+++ src/modules/get_followers_list.py
@@ -14,9 +14,11 @@ def get_followers_list(target_username, headers, output_type):
         headers: Request headers (available from the token module)
         output_type: "file" to save output, "list" to return as a list (default = list)
     """
+    current_date = datetime.now().strftime("%Y-%m-%d_%H;%M") # Get current date and time for the output file naming
+    file_path = f"outputs/({target_username})followers [{current_date}].txt"
+    
     page = 1
     followers_list = []
-    
     while True:
         # per_page = 100, max items per request
         url = f"https://api.github.com/users/{target_username}/followers?per_page=100&page={page}"
@@ -38,15 +40,16 @@ def get_followers_list(target_username, headers, output_type):
 
     if output_type == "file": # "file" or "list" (default=List)
         try:
-            os.makedirs("outputs", exist_ok=True) # Create outputs folder if it doesn't exist
-           
-            current_date = datetime.now().strftime("%Y-%m-%d_%H;%M") # Get current date and time
-            file_path = f"outputs/({target_username})followers [{current_date}].txt"
-            
-            write_in_file(file_path=file_path, input_item=followers_list, writing_mode="w")
-            
-            print(f"Done! All follower usernames saved to {file_path}")
+            if output_type == "file": # "file" or "list" (default=List)
+                try:
+                    os.makedirs("outputs", exist_ok=True) # Create outputs folder if it doesn't exist
+                    write_in_file(file_path=file_path, input_item=followers_list, writing_mode="w")
+
         except Exception as e:
             print(f"Writing in the file was not successful, check this error and try again: \n{e}")
+            
+    if output_type == "file":
+        print(f'SUCCESS: Extracting usernames is done, "{len(followers_list)}" usernames saved to "{file_path}"')
     else:
+        print(f'SUCCESS: Extracting usernames is done, "{len(followers_list)}" usernames saved.')
         return followers_list
