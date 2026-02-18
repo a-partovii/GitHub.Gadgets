928275a11c306416bc403242963c91ed55e1d57f
Ash <partovi.code@gmail.com>
2026-02-18 21:23:46 +0330

    feat_dev: made "check_file_exists" and updated [__init__.py]

--- src/modules/file_modules/__init__.py
+++ src/modules/file_modules/__init__.py
@@ -1,11 +1,11 @@
 from .write_file import write_file
 from .read_file import read_file
 from .filename_handler import unique_filename, filename_date, filename_datetime
-from .check_file_exist import check_file_exist
+from .check_file_exist import check_file_exists
 
 __all__ = ["write_file",
            "read_file",
            "unique_filename", 
            "filename_date", 
            "filename_datetime",
-           "check_file_exist"]
+           "check_file_exists"]
--- src/modules/file_modules/check_file_exist.py
+++ src/modules/file_modules/check_file_exist.py
@@ -1,12 +1,10 @@
 import os
 
-def check_file_exist(file_path:str) -> bool:
+def check_file_exists(file_path:str) -> bool:
     """
     Check if a file exists at the specified path.
 
     Args:
         file_path (str): The path to the file to check.
     """         
-    if os.path.exists(file_path):
-        return True
-    return False
+    return os.path.exists(file_path)
