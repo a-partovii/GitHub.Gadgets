125fd4a8c1c90b002d788effc6feede6451c7281
Ash <partovi.code@gmail.com>
2026-02-20 23:48:59 +0330

    feat_add: update [`config/__init__.py`], add "get_github_username"

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,6 +1,8 @@
 from .tokens import primary_token, secondary_tokens, token_manager, make_headers
+from .get_token_username import get_github_username
 
 __all__ = ["primary_token",
            "secondary_tokens",
            "token_manager",
-           "make_headers"]
+           "make_headers",
+           "get_github_username"]
--- src/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-from .follow import follow
-from .extract_usernames import extract_usernames
-
-__all__ = ["follow",
-           "extract_usernames"]
