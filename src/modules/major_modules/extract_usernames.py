f1076ba7268656dcbc218aa0c7372ac7b2d8276d
Ash <partovi.code@gmail.com>
2026-02-22 03:00:23 +0330

    feat_dev: update error handling and logging in "extract_usernames.py"

--- src/modules/major_modules/extract_usernames.py
+++ src/modules/major_modules/extract_usernames.py
@@ -4,6 +4,7 @@ from modules.utils import delay
 from modules.file_modules import write_file
 from config.tokens import secondary_tokens, token_manager, make_headers
 from modules.file_modules import filename_datetime
+from modules.utils import network_error_handler, response_error_handler
 
 def extract_usernames(target_username:str, source:str, output_type:str ="list"):
     """
@@ -15,14 +16,13 @@ def extract_usernames(target_username:str, source:str, output_type:str ="list"):
         output_type (str): "list" to return as a list (default = list), "file" to save output file too.
 
     Returns:
-        list[str] | None: List of usernames,
-        None if there was an error.
+        list[str] | False: List of usernames, unless there was an critical error
     """
     if output_type == "file":
         file_path = f"outputs/({target_username}){source} {filename_datetime()}"
 
     if source not in {"followers", "following"}:
-        raise ValueError('Invalid argument for "extract_usernames", `source` must be `followers` or `following`')
+        raise ValueError('Invalid argument for {extract_usernames}, `source` must be "followers" or "following"')
     
     page = 1
     usernames_list = []
@@ -47,34 +47,45 @@ def extract_usernames(target_username:str, source:str, output_type:str ="list"):
                 retries += 1
                 delay(message)
 
-        if response.status_code != 200:
-            print(f"Error: HTTP request failed ({response.status_code}): {response.reason}")
-            break
+        status = response.status_code
+        remaining = response.headers.get("X-RateLimit-Remaining", "unknown")
 
-        data_json = response.json()
-        if not data_json:
-            break
+        if status == 200:
+            data_json = response.json()
+            if not data_json:
+                break
 
-        for user in data_json:
-            usernames_list.append(user.get("login"))
+            for user in data_json:
+                usernames_list.append(user.get("login"))
+            message = f'[OK] "{len(usernames_list)}" Usernames Saved '
 
-        if output_type == "file": # "file" or "list" (default=List)
-            try:
-                os.makedirs("outputs", exist_ok=True) # Create outputs folder if it doesn't exist
-                write_file(file_path=file_path, input_item=usernames_list, writing_mode="w")
+            if output_type == "file": # "file" or "list" (default=List)
+                try:
+                    os.makedirs("outputs", exist_ok=True) # Create outputs folder if it doesn't exist
+                    write_file(file_path=file_path, input_item=usernames_list, writing_mode="w")
+                    message += f'to "{file_path}" '
 
-            except Exception as error:
-                print(f"Writing in the file was not successful, check this error and try again: \n{error}")
+                except Exception as error:
+                    message = f"[ERROR] Writing in the File Failed, check this error and try again: \n{error}\n" + message
 
-        page += 1
+            message += f'| Token RateLimit Remaining: "{remaining}"'
+            page += 1
 
-        message = f'SUCCESS: "{len(usernames_list)}" usernames saved'
-        if output_type == "file":
-            message += f' to "{file_path}"'
+        elif status == 404:
+            print(f'[WARN] User "{target_username}" Not Found, check the username and try again.')
+            return False
+
+        else:
+            action, message = response_error_handler(response)
+            if action == "break":
+                print(message)
+                return False
+            
         delay(message ) # Random delay per request (2–6 seconds)
               
     if output_type == "file":
-        print(f'SUCCESS: Extracting usernames is done, "{len(usernames_list)}" usernames saved to "{file_path}"')
+        print(f'[SUCCESS] Extracting Usernames is Done, "{len(usernames_list)}" usernames saved to "{file_path}"\n')
     else:
-        print(f'SUCCESS: Extracting usernames is done, "{len(usernames_list)}" usernames saved.')
+        print(f'[SUCCESS] Extracting Usernames is Done, "{len(usernames_list)}" usernames saved.\n')
+
     return usernames_list
