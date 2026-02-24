5d4e11b71b775cf8522b4e5635b2f708d10884d3
Ash <partovi.code@gmail.com>
2026-02-24 23:36:58 +0330

    style_add: add ascii banner, [`ghg_banner.py`]

---
+++ src/modules/tui/ghg_banner.py
@@ -0,0 +1,21 @@
+
+print(
+"""
+------------------------------------------------------------------------------------------------------------
+|              dddddddddddd                                                                                 |
+|         dddddddddddddddddddddb                           /$$$$$$  /$$   /$$  /$$$$$$                      |
+|      dddd                    dddd                       /$$__  $$| $$  | $$ /$$__  $$                     |
+|   dddd    dddd   ____   dddd    dddb                   | $$  \__/| $$  | $$| $$  \__/                     |
+|  dddd    dddddddddddddddddddb    dddd                  | $$ /$$$$| $$$$$$$$| $$ /$$$$                     |
+|  ddd    dddddddddddddddddddddb    ddd                  | $$|_  $$| $$__  $$| $$|_  $$                     |
+| ddb     dddddddddddddddddddddd     ddd                 | $$  \ $$| $$  | $$| $$  \ $$                     |
+| ddd     dddddddddddddddddddddd     ddd                 |  $$$$$$/| $$  | $$|  $$$$$$/                     |
+| bdd      dddddddddddddddddddb      ddd                  \______/ |__/  |__/ \______/                      |
+|  ddd       bddddddddddddddb       ddd  ____ _ _   _   _       _      ____           _            _        |
+|  dddb  \dd\   pddddddddb        dddd  / ___(_) |_| | | |_   _| |__  / ___| __ _  __| | __ _  ___| |_ ___  |
+|   dddd   \dd\_dddddddddb       dddd  | |  _| | __| |_| | | | | '_ \| |  _ / _` |/ _` |/ _` |/ _ \ __/ __| |
+|      dddd  \pddbdddddddd    dddd     | |_| | | |_|  _  | |_| | |_) | |_| | (_| | (_| | (_| |  __/ |_\__ \\ |
+|         ddddddddddddddddddddd         \____|_|\__|_| |_|\__,_|_.__(_)____|\__,_|\__,_|\__, |\___|\__|___/ |
+|              dddddddddddd                                                             |___/               |
+-------------------------------------------------------------------------------------------------------------
+""")
