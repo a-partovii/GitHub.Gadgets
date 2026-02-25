3590e86d30d73226e66aad18a03dc457ae35fbf4
Ash <partovi.code@gmail.com>
2026-02-25 22:30:10 +0330

    feat_newFile: add [`__init__.py`] to operations folder

---
+++ src/modules/operations/__init__.py
@@ -0,0 +1,10 @@
+from .follow_operation import *
+
+__all__ = ["follow_back",
+           "follow_from_file",
+           "follow_from_followers",
+           "follow_from_following",
+           "follow_my_stargazers",
+           "follow_repo_stargazers",
+           "follow_bulk",
+           "follow_submenu"]
