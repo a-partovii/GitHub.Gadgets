3a25e6b8a338a85a21768e9d3c45d8afb8ef50cc
Ash <partovi.code@gmail.com>
2026-02-25 16:57:36 +0330

    feat_add: add "follow_from_following "

--- src/modules/operations/follow_operations.py
+++ src/modules/operations/follow_operations.py
@@ -32,13 +32,21 @@ def follow_from_followers(target_username:str):
         follow(usernames)
     except:
         pass
-    
+
+# -----------------------------------------------------------------------------------------
+def follow_from_following(target_username:str):
+    try:
+        usernames = extract_usernames(target_username, "following", show_message=False)
+        follow(usernames)
+    except:
+        pass
+
 # -----------------------------------------------------------------------------------------
 follow_sub_menu = {
     "1": {"label": "Follow back your followers", "action": follow_back},
     "2": {"label": "Follow from a list file", "action": follow_from_file},
     "3": {"label": "Follow from a user's followers", "action": follow_from_followers},
-    # "4": {"label": "Follow from a user's following", "action": follow_from_following},
+    "4": {"label": "Follow from a user's following", "action": follow_from_following},
     # "5": {"label": "Follow who starred your repositories", "action": follow_my_stargazers},
     # "6": {"label": "Follow who starred a given repository", "action": follow_repo_stargazers},
     # "7": {"label": "Bulk follow with a count limit", "action": follow_bulk},
