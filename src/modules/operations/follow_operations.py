ebfcecd2a9bf8978bd8f6c9394a5003fb0da44a1
Ash <partovi.code@gmail.com>
2026-02-25 17:00:55 +0330

    feat_dev: pass "follow_repo_stargazers" and "follow_my_stargazers"

--- src/modules/operations/follow_operations.py
+++ src/modules/operations/follow_operations.py
@@ -42,13 +42,21 @@ def follow_from_following(target_username:str):
         pass
 
 # -----------------------------------------------------------------------------------------
+def follow_my_stargazers():
+    print("coming soon...")
+
+# -----------------------------------------------------------------------------------------
+def follow_repo_stargazers():
+    print("coming soon...")
+
+# -----------------------------------------------------------------------------------------
 follow_sub_menu = {
     "1": {"label": "Follow back your followers", "action": follow_back},
     "2": {"label": "Follow from a list file", "action": follow_from_file},
     "3": {"label": "Follow from a user's followers", "action": follow_from_followers},
     "4": {"label": "Follow from a user's following", "action": follow_from_following},
-    # "5": {"label": "Follow who starred your repositories", "action": follow_my_stargazers},
-    # "6": {"label": "Follow who starred a given repository", "action": follow_repo_stargazers},
+    "5": {"label": "Follow who starred your repositories", "action": follow_my_stargazers},
+    "6": {"label": "Follow who starred a given repository", "action": follow_repo_stargazers},
     # "7": {"label": "Bulk follow with a count limit", "action": follow_bulk},
     # "0": {"label": "Back to main menu", "action": return_to_main},
 }
