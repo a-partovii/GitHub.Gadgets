6ad5fb1a63b2257df9a8b61e20bea7e25059ce97
Ash <partovi.code@gmail.com>
2026-02-26 16:54:21 +0330

    feat_dev: update [`__init__.py`], add "extract_bulk_usernames"

--- src/modules/major_modules/__init__.py
+++ src/modules/major_modules/__init__.py
@@ -2,10 +2,12 @@ from .follow import follow, continue_follow_progress
 from .unfollow import unfollow, continue_unfollow_progress
 from .extract_usernames import extract_usernames
 from .send_request import send_request
-
-__all__ = ["follow",
-           "unfollow"
+from .extract_bulk_usernames import extract_bulk_usernames
+__all__ = ["send_request",
            "extract_usernames",
-           "send_request",
+           "extract_bulk_usernames",
+           "follow",
            "continue_follow_progress",
-           "continue_unfollow_progress"]
+           "unfollow",
+           "continue_unfollow_progress"
+           ]
