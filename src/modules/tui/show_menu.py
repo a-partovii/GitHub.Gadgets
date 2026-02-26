3ffb25dde1e1151348b16750e440995b7cde9495
Ash <partovi.code@gmail.com>
2026-02-26 18:28:52 +0330

    feat_dev: improve input validation to "show_menu"

--- src/modules/tui/show_menu.py
+++ src/modules/tui/show_menu.py
@@ -1,13 +1,31 @@
-def show_menu(menu_dict):
-    while True:
-        print("Choose an option to continue:")
+def show_menu(menu_dict:dict) :
+    """
+    Display a menu and execute the selected action.
 
+    Args:
+        menu_dict (dict): Dictionary of menu options.
+            e.g. Format: 
+            {
+                "1": {"label": "Option name", "action": callable},
+                ...
+            }
+
+    Notice: For functions that require arguments, use a lambda in the action field
+    """
+    while True:
+        # Display menu options
+        print("Choose an option to continue:\n")
         for key, item in menu_dict.items():
             print(f"{key}. {item['label']}")
 
-        choice = input("\nEnter your choice: ").strip()
+        print()
+        while True: # Ask user until a valid choice
+            choice = input("Enter your choice: ").strip()
 
-        if choice in menu_dict:
-            menu_dict[choice]["action"]()
-        else:
-            print("[ERROR] Invalid option. Please try again.")
+            if choice in menu_dict:
+                break
+            else:
+                print("Please just enter a valid option number and press <Enter>.")
+
+        # Execute selected action
+        menu_dict[choice]["action"]()
