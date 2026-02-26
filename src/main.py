1f007a142a2ec8cdec53e88d2763fde3fe01b491
Ash <partovi.code@gmail.com>
2026-02-26 19:33:15 +0330

    feat_dev: 'follow_submenu' implented to main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
-from inputs.tokens import primary_token, secondary_tokens, random_token, make_headers
-from modules.get_followers_list import get_followers_list
+from modules.tui import show_menu
+from modules.operations import follow_submenu
 
-token = secondary_tokens[""]
-header = make_headers(token)
+main_menu = {
+    "1": {"label": "Follow", "action": lambda: show_menu(follow_submenu)},
+    # "2": {"label": "Unfollow", "action": "unfollow"},
+    # "3": {"label": "Extract usernames", "action": ""},
+    # "4": {"label": "Adjust Blacklist", "action": ""},
+    # "5": {"label": "Adjust Whitelist", "action": ""},
+    # "6": {"label": "Adjust GitHub tokens", "action": ""},
+    # "7": {"label": "Visit credits", "action": ""},
+    # "0": {"label": "Exit", "action": "exit_app"},
+}
 
+show_menu(main_menu)
