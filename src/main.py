2885edc813d582d3d49f76c327949d68e24d9aa9
Ash <partovi.code@gmail.com>
2026-02-27 17:18:23 +0330

    feat_add: add "unfollow_submenu" to [`main.py`]

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from modules.tui import show_menu
-from modules.operations import follow_submenu
-
+from modules.operations import (
+                            follow_submenu,
+                            unfollow_submenu)
+                            
 main_menu = {
     "1": {"label": "Follow", "action": lambda: show_menu(follow_submenu)},
-    # "2": {"label": "Unfollow", "action": "unfollow"},
+    "2": {"label": "Unfollow", "action": lambda:show_menu(unfollow_submenu)},
     # "3": {"label": "Extract usernames", "action": ""},
     # "4": {"label": "Adjust Blacklist", "action": ""},
     # "5": {"label": "Adjust Whitelist", "action": ""},
