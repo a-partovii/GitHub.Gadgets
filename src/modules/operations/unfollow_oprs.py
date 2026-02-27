b001df28e18240a2e6850f4a7dc8ea2abf2418b0
Ash <partovi.code@gmail.com>
2026-02-27 17:30:37 +0330

    feat_add: add 'unfollow_submenu' to[`unfollow_oprs.py`]

---
+++ src/modules/operations/unfollow_oprs.py
@@ -0,0 +1,10 @@
+unfollow_submenu = {
+    "1": {"label": "Unfollow users who don't follow you back", "action": unfollow_non_followers},
+    "2": {"label": "Unfollow from a list file", "action": unfollow_from_file},
+    "3": {"label": "Unfollow from a user's followers", "action": unfollow_from_followers},
+    "4": {"label": "Unfollow from a user's following", "action": unfollow_from_following},
+    "5": {"label": "Unfollow who starred a given user's repositories", "action": unfollow_user_stargazers},
+    "6": {"label": "Unfollow who starred a given repository", "action": unfollow_repo_stargazers},
+    "7": {"label": "Bulk unfollow with a limit count", "action": unfollow_bulk},
+    "0": {"label": "Back to main menu", "action": return_to_main},
+}
