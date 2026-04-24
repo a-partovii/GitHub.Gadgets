538eb9cb4139c29d048e3e6f0d226279baf18fed
Ash <partovi.code@gmail.com>
2026-04-24 17:32:51 +0330

    feat_add: "unfollow_from_following" func

--- src/modules/operations/unfollow_oprs.py
+++ src/modules/operations/unfollow_oprs.py
@@ -39,11 +39,20 @@ def unfollow_from_followers():
         pass
 
 # -----------------------------------------------------------------------------------------
+def unfollow_from_following():
+    try:
+        target_username = input("Enter a username to unfollow their following: ")
+        usernames = extract_usernames(target_username, "following", show_message=False)
+        unfollow(usernames)
+    except:
+        pass
+
+# -----------------------------------------------------------------------------------------
 unfollow_submenu = {
     "1": {"label": "Unfollow users who don't follow you back", "action": unfollow_non_followers},
     "2": {"label": "Unfollow from a list file", "action": unfollow_from_file},
-#     "3": {"label": "Unfollow from a user's followers", "action": unfollow_from_followers},
-#     "4": {"label": "Unfollow from a user's following", "action": unfollow_from_following},
+    "3": {"label": "Unfollow from a user's followers", "action": unfollow_from_followers},
+    "4": {"label": "Unfollow from a user's following", "action": unfollow_from_following},
 #     "5": {"label": "Unfollow who starred a given user's repositories", "action": unfollow_user_stargazers},
 #     "6": {"label": "Unfollow who starred a given repository", "action": unfollow_repo_stargazers},
 #     "7": {"label": "Bulk unfollow with a limit count", "action": unfollow_bulk},
