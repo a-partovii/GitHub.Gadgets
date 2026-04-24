c4e84a5a6452d7fffb9e9ec38f1b3a44ca318cfa
Ash <partovi.code@gmail.com>
2026-04-24 17:29:01 +0330

    feat_add: "unfollow_from_file" func

--- src/modules/operations/unfollow_oprs.py
+++ src/modules/operations/unfollow_oprs.py
@@ -1,4 +1,4 @@
-from modules.major_modules import unfollow, extract_usernames, extract_bulk_usernames_to_follow
+from modules.major_modules import unfollow, extract_usernames
 from modules.file_modules import file_picker, read_file
 from modules.utils import filter_list
 from config.tokens import primary_token
@@ -15,13 +15,28 @@ def unfollow_non_followers():
         pass
 
 # -----------------------------------------------------------------------------------------
+def unfollow_from_file():
+    try:
+        print("If you can't see the file picker window, check behind this window.")
+        file_path = file_picker()
+        if not file_path:
+            print("Operation cancelled.")
+            return
+        
+        usernames = read_file(file_path)
+        unfollow(usernames)
+
+    except Exception as error:
+        print(f"[ERROR] {error}")
+
+# -----------------------------------------------------------------------------------------
 unfollow_submenu = {
     "1": {"label": "Unfollow users who don't follow you back", "action": unfollow_non_followers},
     "2": {"label": "Unfollow from a list file", "action": unfollow_from_file},
-    "3": {"label": "Unfollow from a user's followers", "action": unfollow_from_followers},
-    "4": {"label": "Unfollow from a user's following", "action": unfollow_from_following},
-    "5": {"label": "Unfollow who starred a given user's repositories", "action": unfollow_user_stargazers},
-    "6": {"label": "Unfollow who starred a given repository", "action": unfollow_repo_stargazers},
-    "7": {"label": "Bulk unfollow with a limit count", "action": unfollow_bulk},
-    "0": {"label": "Back to main menu", "action": return_to_main},
+#     "3": {"label": "Unfollow from a user's followers", "action": unfollow_from_followers},
+#     "4": {"label": "Unfollow from a user's following", "action": unfollow_from_following},
+#     "5": {"label": "Unfollow who starred a given user's repositories", "action": unfollow_user_stargazers},
+#     "6": {"label": "Unfollow who starred a given repository", "action": unfollow_repo_stargazers},
+#     "7": {"label": "Bulk unfollow with a limit count", "action": unfollow_bulk},
+#     "0": {"label": "Back to main menu", "action": return_to_main},
 }
