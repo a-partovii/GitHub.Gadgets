dda7ab7ebc7a4d2165d42a5c146065527b399d4b
Ash <partovi.code@gmail.com>
2026-04-24 17:47:53 +0330

    feat_dev: update [`__init__.py`], add unfollow oprs

--- src/modules/operations/__init__.py
+++ src/modules/operations/__init__.py
@@ -1,4 +1,5 @@
 from .follow_oprs import *
+from .unfollow_oprs import *
 
 __all__ = ["follow_back",
            "follow_from_file",
@@ -7,4 +8,13 @@ __all__ = ["follow_back",
            "follow_my_stargazers",
            "follow_repo_stargazers",
            "follow_bulk",
-           "follow_submenu"]
+           "follow_submenu"
+           # ----------------
+           "unfollow_non_followers",
+           "unfollow_from_file",
+           "unfollow_from_followers",
+           "unfollow_from_following",
+           "unfollow_repo_stargazers",
+           "unfollow_my_stargazers",
+           "unfollow_submenu"
+           ]
