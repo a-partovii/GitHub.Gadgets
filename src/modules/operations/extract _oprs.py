5ace83deb951032fc4ed90f7b75553f6afc7a26e
Ash <partovi.code@gmail.com>
2026-04-29 16:12:19 +0330

    feat_init: new file [`extract_oprs.py`], add 'submenu'

---
+++ src/modules/operations/extract _oprs.py
@@ -0,0 +1,11 @@
+extract_submenu = {
+    # "1": {"label": "Extract your followers usernames", "action": extract_your_followers},
+    # "2": {"label": "Extract your following usernames", "action": extract_your_following},
+    # "3": {"label": "Extract a user's followers", "action": extract_from_followers},
+    # "4": {"label": "Extract a user's following", "action": extract_from_following},
+    # "5": {"label": "Extract usernames of users who starred your repositories", "action": extract_my_stargazers},
+    # "6": {"label": "Extract usernames of users who starred a given user's repositories", "action": extract_user_stargazers},
+    # "7": {"label": "Extract usernames of users who starred a given repository", "action": extract_repo_stargazers},
+    # "8": {"label": "Extract bulk usernames to follow with a limit", "action": extract_bulk},
+    # "0": {"label": "Back to main menu", "action": return_to_main},
+}
